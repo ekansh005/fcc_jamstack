90a4e7302a67a0309498bc0e88fbcfb39eed4ce9
Ekansh Saxena <ekansh005@gmail.com>
2021-09-09 14:28:43 +0300

    Get news for few countries

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,25 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require("dotenv").config();
 
-module.exports = async function () {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
-    return response.data;
+    return {
+      country,
+      articles: response.data.articles,
+    };
   } catch (error) {
     console.log(error);
   }
+}
+
+module.exports = async function () {
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then((newsObjects) => {
+    console.log(newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
