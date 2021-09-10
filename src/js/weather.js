38ff2f1d7dc002ed0e17f7966bb483fdb7ae6075
Ekansh Saxena <ekansh005@gmail.com>
2021-09-10 12:33:57 +0300

    Add function

---
+++ src/js/weather.js
@@ -0,0 +1,16 @@
+function success(position) {
+  const coordinates = position.coords;
+  const url = `/.netlify/functions/weatherapi?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      //dom manipulation to update weather information
+      document.querySelector("#area").textContent(`data.name`);
+      document.querySelector("#temp").textContent(`data.main.temp`);
+      document.querySelector("#condition").textContent(`data.weather[0].main`);
+      document.querySelector("#desc").textContent(`data.weather[0].description`);
+      document.querySelector("#weather").classList.remove("hidden");
+    });
+}
